c2b748f02dda29ac95732e486f167f22acb5fd4c
anand0412 <xsslgr1007@gmail.com>
2026-07-17 11:47:36 +0530

    3

--- assets/js/attendance.js
+++ assets/js/attendance.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("Attendance Page Loaded");
+    console.log("Attendance JS Loaded");
+
+    const openBtn =
+        document.getElementById("markAttendanceBtn");
+
+    const modal =
+        document.getElementById("attendanceModal");
+
+    const closeBtn =
+        document.getElementById("closeAttendanceModal");
+
+    openBtn.addEventListener("click", () => {
+
+        modal.style.display = "flex";
+
+    });
+
+    closeBtn.addEventListener("click", () => {
+
+        modal.style.display = "none";
+
+    });
 
 });
