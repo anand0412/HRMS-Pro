7f0370427435bd14ef1e90f3516ae2cb87b46e28
anand0412 <xsslgr1007@gmail.com>
2026-07-30 17:58:20 +0530

    s

--- assets/js/service-book.js
+++ assets/js/service-book.js
@@ -104,3 +104,23 @@ tabButtons.forEach(button=>{
     });
 
 });
+
+const tabButtons=document.querySelectorAll(".tab-btn");
+
+const tabContents=document.querySelectorAll(".tab-content");
+
+tabButtons.forEach(button=>{
+
+    button.addEventListener("click",()=>{
+
+        tabButtons.forEach(btn=>btn.classList.remove("active"));
+
+        tabContents.forEach(tab=>tab.classList.remove("active"));
+
+        button.classList.add("active");
+
+        document.getElementById(button.dataset.tab).classList.add("active");
+
+    });
+
+});
